b71fec54f532d89b84442df77f05ec25d4551482
Wade Salazar <Wade@KenTender.com>
2016-12-28 00:03:19 -0600

    cleaned up UI components and var scopes

--- nifi-opcua-bundle/nifi-opcua-bundle-processors/src/main/java/com/kentender/nifi/nifi_opcua_bundle/GetNodeIds.java
+++ nifi-opcua-bundle/nifi-opcua-bundle-processors/src/main/java/com/kentender/nifi/nifi_opcua_bundle/GetNodeIds.java
@@ -1,33 +1,12 @@
 package com.kentender.nifi.nifi_opcua_bundle;
 
-import static org.opcfoundation.ua.utils.EndpointUtil.selectByProtocol;
-import static org.opcfoundation.ua.utils.EndpointUtil.selectBySecurityPolicy;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.net.InetAddress;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.InvalidParameterSpecException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
 import java.util.Set;
-import java.util.UUID;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-
-import org.apache.nifi.annotation.behavior.InputRequirement;
 import org.apache.nifi.annotation.behavior.ReadsAttribute;
 import org.apache.nifi.annotation.behavior.ReadsAttributes;
 import org.apache.nifi.annotation.behavior.WritesAttribute;
@@ -47,29 +26,9 @@ import org.apache.nifi.processor.Relationship;
 import org.apache.nifi.processor.exception.ProcessException;
 import org.apache.nifi.processor.io.OutputStreamCallback;
 import org.apache.nifi.processor.util.StandardValidators;
-import org.opcfoundation.ua.application.Client;
-import org.opcfoundation.ua.application.SessionChannel;
 import org.opcfoundation.ua.builtintypes.ExpandedNodeId;
-import org.opcfoundation.ua.builtintypes.LocalizedText;
 import org.opcfoundation.ua.builtintypes.NodeId;
-import org.opcfoundation.ua.builtintypes.UnsignedInteger;
-import org.opcfoundation.ua.common.ServiceFaultException;
-import org.opcfoundation.ua.common.ServiceResultException;
-import org.opcfoundation.ua.core.BrowseDescription;
-import org.opcfoundation.ua.core.BrowseDirection;
-import org.opcfoundation.ua.core.BrowseRequest;
-import org.opcfoundation.ua.core.BrowseResponse;
-import org.opcfoundation.ua.core.BrowseResult;
-import org.opcfoundation.ua.core.EndpointDescription;
-import org.opcfoundation.ua.core.IdType;
 import org.opcfoundation.ua.core.Identifiers;
-import org.opcfoundation.ua.core.ReferenceDescription;
-import org.opcfoundation.ua.transport.security.Cert;
-import org.opcfoundation.ua.transport.security.KeyPair;
-import org.opcfoundation.ua.transport.security.PrivKey;
-import org.opcfoundation.ua.transport.security.SecurityPolicy;
-import org.opcfoundation.ua.utils.CertificateUtils;
-
 import com.kentender.nifi.nifi_opcua_services.OPCUAService;
 
 @Tags({"OPC", "OPCUA", "UA"})
@@ -78,45 +37,18 @@ import com.kentender.nifi.nifi_opcua_services.OPCUAService;
 @ReadsAttributes({@ReadsAttribute(attribute="", description="")})
 @WritesAttributes({@WritesAttribute(attribute="", description="")})
 
-public class GetExpandedNodeIds extends AbstractProcessor {
+public class GetNodeIds extends AbstractProcessor {
 	
+	private static String starting_node = null;
+	private static String print_indentation = "No";
+	private static Integer max_recursiveDepth;
 	
-	// TODO clean up static vars and implement private where needed
-	final Locale ENGLISH = Locale.ENGLISH;
-	static int max_recursiveDepth = 0;
-	static int recursiveDepth = 0;
-	
-	static String url = "";
-	static String applicationName = "";
-	static KeyPair myClientApplicationInstanceCertificate = null;
-	static KeyPair myHttpsCertificate = null;
-	static String outputFilename = null;
-	static String print_indentation = null;
-	static String starting_node = null;
-	static EndpointDescription[] endpoints = null;
-	static Client myClient = null;
-
 	public static final PropertyDescriptor OPCUA_SERVICE = new PropertyDescriptor.Builder()
 			  .name("OPC UA Service")
 			  .description("Specifies the OPC UA Service that can be used to access data")
 			  .required(true)
 			  .identifiesControllerService(OPCUAService.class)
 			  .build();
-	
-	public static final PropertyDescriptor ENDPOINT = new PropertyDescriptor
-            .Builder().name("Endpoint URL")
-            .description("the opc.tcp address of the opc ua server")
-            .required(true)
-            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
-            .build();
-    
-    public static final PropertyDescriptor SECURITY_POLICY = new PropertyDescriptor
-            .Builder().name("Security Policy")
-            .description("How should Nifi authenticate with the UA server")
-            .required(true)
-            .allowableValues("None", "Basic128Rsa15", "Basic256", "Basic256Rsa256")
-            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
-            .build();
     
     public static final PropertyDescriptor STARTING_NODE = new PropertyDescriptor
             .Builder().name("Starting Node")
@@ -126,24 +58,11 @@ public class GetExpandedNodeIds extends AbstractProcessor {
     
     public static final PropertyDescriptor RECURSIVE_DEPTH = new PropertyDescriptor
             .Builder().name("Recursive Depth")
-            .description("Maxium depth from the starting node to read, Default is 1")
+            .description("Maxium depth from the starting node to read, Default is 0")
             .required(true)
             .addValidator(StandardValidators.INTEGER_VALIDATOR)
             .build();
     
-    public static final PropertyDescriptor APPLICATION_NAME = new PropertyDescriptor
-            .Builder().name("Application Name")
-            .description("The application name is used to label certificates identifying this application")
-            .required(true)
-            .addValidator(StandardValidators.NON_BLANK_VALIDATOR)
-            .build();
-    
-    public static final PropertyDescriptor OUTFILE_NAME = new PropertyDescriptor
-            .Builder().name("Output filename")
-            .description("File path and name used for output")
-            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
-            .build();
-	
     public static final PropertyDescriptor PRINT_INDENTATION = new PropertyDescriptor
             .Builder().name("Print Indentation")
             .description("Should Nifi add indentation to the output text")
@@ -171,12 +90,8 @@ public class GetExpandedNodeIds extends AbstractProcessor {
     protected void init(final ProcessorInitializationContext context) {
         final List<PropertyDescriptor> descriptors = new ArrayList<PropertyDescriptor>();
         descriptors.add(OPCUA_SERVICE);
-        descriptors.add(ENDPOINT);
         descriptors.add(RECURSIVE_DEPTH);
-        descriptors.add(SECURITY_POLICY);
         descriptors.add(STARTING_NODE);
-        descriptors.add(APPLICATION_NAME);
-        descriptors.add(OUTFILE_NAME);
         descriptors.add(PRINT_INDENTATION);
 
         this.descriptors = Collections.unmodifiableList(descriptors);
@@ -199,15 +114,9 @@ public class GetExpandedNodeIds extends AbstractProcessor {
 
     @OnScheduled
     public void onScheduled(final ProcessContext context) {
-    	
-    	final ComponentLog logger = getLogger();
 		
-    	// Set variables
-    	applicationName = context.getProperty(APPLICATION_NAME).getValue();
-    	outputFilename = context.getProperty(OUTFILE_NAME).getValue();
 		print_indentation = context.getProperty(PRINT_INDENTATION).getValue();
 		max_recursiveDepth = Integer.valueOf(context.getProperty(RECURSIVE_DEPTH).getValue());
-		url = context.getProperty(ENDPOINT).getValue();
 		starting_node = context.getProperty(STARTING_NODE).getValue();
 				
     }
@@ -248,8 +157,6 @@ public class GetExpandedNodeIds extends AbstractProcessor {
 		// Transfer data to flow file
         session.transfer(flowFile, SUCCESS);
         
-        // Reset our stringBuilder
-        stringBuilder.setLength(0);
 	}
 	
 
